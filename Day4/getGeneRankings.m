cdaad6e613c82ccafeb389b233470ea9463c0ae9
Chris Tralie <chris.tralie@gmail.com>
2017-07-13 16:14:58 -0400

    Adding gene rankings code

---
+++ Day4/getGeneRankings.m
@@ -0,0 +1,13 @@
+addpath('../GeometryTools');
+addpath('../ripser');
+addpath('../TDETools');
+
+load('subsample_GeneData.mat');
+X = subsampleGeneData;
+N = size(X, 1);
+scores = zeros(N, 1);
+
+parfor ii = 1:N
+    ii
+    scores(ii) = NaivePeriodicityScoring(X(ii, :));
+end
